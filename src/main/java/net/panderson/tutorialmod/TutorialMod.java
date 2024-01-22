3071fadd9ef5e1333484d993aaec027b3c99a6e2
YellowRockCo <endersonthecoolguy@gmail.com>
2024-01-22 15:35:05 -0600

    actual item not working?

--- src/main/java/net/panderson/tutorialmod/TutorialMod.java
+++ src/main/java/net/panderson/tutorialmod/TutorialMod.java
@@ -2,16 +2,18 @@ package net.panderson.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.panderson.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-    public static final String MOD_ID = "tutorial_mod";
+    public static final String MOD_ID = "tutorialmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
 	}
 }
