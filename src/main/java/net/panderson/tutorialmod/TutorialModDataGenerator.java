6299bad16619e1ed935a2233c41b1965c078f48c
YellowRockCo <endersonthecoolguy@gmail.com>
2024-01-24 20:02:53 -0600

    data gen no work so using json afterall

--- src/main/java/net/panderson/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/net/panderson/tutorialmod/TutorialModDataGenerator.java
@@ -10,11 +10,6 @@ public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-		pack.addProvider(ModBlockTagProvider::new);
-		pack.addProvider(ModItemTagProvider::new);
-		pack.addProvider(ModLootTableProvider::new);
-		pack.addProvider(ModModelProvider::new);
-		pack.addProvider(ModRecipeProvider::new);
 
 
 	}
--- src/main/java/net/panderson/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/panderson/tutorialmod/block/ModBlocks.java
@@ -8,7 +8,6 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.panderson.tutorialmod.TutorialMod;
 
